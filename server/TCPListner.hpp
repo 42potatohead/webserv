87ec9371f8e37aec5c0318005b900493b3820676
42potatohead <zaid42amman@gmail.com>
2026-09-06 22:26:56 +0300

    support for non-blocking post cgi

--- server/TCPListner.hpp
+++ server/TCPListner.hpp
@@ -22,9 +22,8 @@ class TCPListner {
         std::map<int, ServerConfig> listeningSockets;
         std::vector<struct pollfd> fds;
 		std::map<int, Client> clients;
-		std::map<int, int> cgiToClient;
 		std::map<int, int> cgiToClient;		// Existing: Maps output pipe -> client
-    std::map<int, int> cgiInToClient; 		// <-- NEW: Maps input pipe -> client
+		std::map<int, int> cgiInToClient; 	// Maps input pipe -> client
 	public:
 		TCPListner(const std::vector<ServerConfig>& configs);
 		~TCPListner();
